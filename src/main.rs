fd56cb70056003bc647afcfb68e2816a989bc2aa
Judah High <judah.high@protonmail.com>
2023-09-11 17:07:26 -0600

    Created endpoints for performing POST and GET requests against contacts for a given user of the contacts app.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use bcrypt::{ DEFAULT_COST, hash, verify };
 #[allow(warnings, unused)]
 mod prisma;
 use prisma::PrismaClient;
-use prisma::user;
+use prisma::{ user, contact };
 
 fn is_authed(req: HttpRequest) -> bool {
     match req.headers().get("Authorization") {
@@ -199,6 +199,85 @@ async fn delete_user(
     HttpResponse::Ok().json(user)
 }
 
+#[derive(Deserialize)]
+struct CreateContactRequest {
+    first_name: String,
+    last_name: String,
+    email: String,
+    tel: String,
+}
+
+#[post("/users/{user_id}/contacts")]
+async fn create_contact(
+    client: web::Data<Arc<PrismaClient>>,
+    body: web::Json<CreateContactRequest>,
+    path: web::Path<String>,
+    req: HttpRequest
+) -> impl Responder {
+    if !is_authed(req) {
+        return HttpResponse::Unauthorized().finish();
+    }
+
+    let user_id: String = path.into_inner();
+    log::info!("{}", user_id);
+    let user = client
+        .contact()
+        .create(
+            body.first_name.to_string(),
+            body.last_name.to_string(),
+            body.email.to_string(),
+            body.tel.to_string(),
+            user::id::equals(user_id.to_string()),
+            vec![]
+        )
+        .exec().await
+        .unwrap();
+
+    HttpResponse::Ok().json(user)
+}
+
+#[get("/users/{user_id}/contacts/{contact_id}")]
+async fn get_contact(
+    client: web::Data<Arc<PrismaClient>>,
+    path: web::Path<(String, String)>,
+    req: HttpRequest
+) -> impl Responder {
+    if !is_authed(req) {
+        return HttpResponse::Unauthorized().finish();
+    }
+
+    let (user_id, contact_id) = path.into_inner();
+    match
+        client
+            .contact()
+            .find_first(vec![contact::id::equals(contact_id), contact::user_id::equals(user_id)])
+            .exec().await
+            .unwrap()
+    {
+        Some(user_data) => HttpResponse::Ok().content_type("application/json").json(user_data),
+        None => HttpResponse::NotFound().finish(),
+    }
+}
+
+#[get("/users/{user_id}/contacts")]
+async fn get_all_contacts(
+    client: web::Data<Arc<PrismaClient>>,
+    path: web::Path<String>,
+    req: HttpRequest
+) -> impl Responder {
+    if !is_authed(req) {
+        return HttpResponse::Unauthorized().finish();
+    }
+
+    let user_id = path.into_inner();
+    let data = client
+        .contact()
+        .find_many(vec![contact::user_id::equals(user_id.to_string())])
+        .exec().await
+        .unwrap();
+    HttpResponse::Ok().content_type("application/json").json(data)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
@@ -230,6 +309,9 @@ async fn main() -> std::io::Result<()> {
             .service(get_user)
             .service(update_user)
             .service(delete_user)
+            .service(create_contact)
+            .service(get_contact)
+            .service(get_all_contacts)
     })
         .bind(host)?
         .run().await
